f095301ab78d12cd8ea9802405193d8338cc8eaf
taijusanagi <taijusanagi@gmail.com>
2023-10-08 13:44:50 +0900

    modify mock of paymaster

--- app/src/app/paymaster/route.ts
+++ app/src/app/paymaster/route.ts
@@ -2,6 +2,6 @@ import { type NextRequest } from "next/server";
 
 //TODO: integrate SSI SDK and signature
 export async function POST(request: NextRequest) {
-  const credential = "credential";
-  return Response.json({ credential });
+  const signature = "signature";
+  return Response.json({ signature });
 }
